250b04ec0af2ca9f36ab4c9ba64838b6908251c7
LEBEAU Christophe <c.lebeau23@gmail.com>
2015-08-17 23:16:42 +0200

    Moved the algorithm to the SpeechParser class. Added methods to retrieve specific objets from a database

--- CalliopeEngine/src/com/rokuan/calliopecore/parser/WordDatabase.java
+++ CalliopeEngine/src/com/rokuan/calliopecore/parser/WordDatabase.java
@@ -1,12 +1,31 @@
 package com.rokuan.calliopecore.parser;
 
-import com.rokuan.calliopecore.sentence.Verb;
+import com.rokuan.calliopecore.sentence.CityInfo;
+import com.rokuan.calliopecore.sentence.ColorInfo;
+import com.rokuan.calliopecore.sentence.CountryInfo;
+import com.rokuan.calliopecore.sentence.CustomObject;
+import com.rokuan.calliopecore.sentence.CustomPlace;
+import com.rokuan.calliopecore.sentence.LanguageInfo;
+import com.rokuan.calliopecore.sentence.PlacePreposition;
+import com.rokuan.calliopecore.sentence.PurposePreposition;
+import com.rokuan.calliopecore.sentence.TimePreposition;
 import com.rokuan.calliopecore.sentence.VerbConjugation;
+import com.rokuan.calliopecore.sentence.WayPreposition;
 import com.rokuan.calliopecore.sentence.Word;
 
 public interface WordDatabase {
-	Word findWord(String word);
-	//Verb<?> getVerb(String verbName);
-	Verb getVerb(String verbName);
-	VerbConjugation findConjugatedVerb(String conjugatedVerb);
+	boolean wordStartsWith(String q);
+
+	Word findWord(String q);
+	LanguageInfo findLanguageInfo(String q);
+	ColorInfo findColorInfo(String q);
+	CityInfo findCityInfo(String q);
+	CountryInfo findCountryInfo(String q);
+	CustomObject findCustomObject(String q);
+	CustomPlace findCustomPlace(String q);
+	PlacePreposition findPlacePreposition(String q);
+	TimePreposition findTimePreposition(String q);
+	WayPreposition findWayPreposition(String q);
+	PurposePreposition findPurposePreposition(String q);
+	VerbConjugation findConjugation(String q);
 }
